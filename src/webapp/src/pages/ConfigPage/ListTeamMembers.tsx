d13fabd1418709c1bb92f9f77f60d2eab6ff6288
Daniel Krawciw <daniel.krawciw@gmail.com>
2024-08-03 19:53:08 -0600

    Very small change to make the delete button in the list elements not activate by pressing enter

--- src/webapp/src/pages/ConfigPage/ListTeamMembers.tsx
+++ src/webapp/src/pages/ConfigPage/ListTeamMembers.tsx
@@ -8,7 +8,7 @@ export const ListTeamMembers = (props: Props) => {
     return (
         <ul className="list-group" data-bs-theme="light">
             {props.teamMemberNames.map( (name, idx) => {
-                return <li key={idx} className="list-group-item quiz-list">{name} <button className="btn btn-outline-danger" onClick={ () => {props.removeTeamMemberFunc(props.teamId,name)} }>Delete</button></li>
+                return <li key={idx} className="list-group-item quiz-list">{name} <button className="btn btn-outline-danger" onClick={ () => {props.removeTeamMemberFunc(props.teamId,name)} } type="button">Delete</button></li>
             } )}
         </ul>
     );
